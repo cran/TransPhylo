8640e51cf4b188631898158923a2f9e5bcb4a7f6
Xavier Didelot <xavier.didelot@gmail.com>
2021-04-20 12:30:03 +0000

    version 1.4.5

--- inst/doc/multitree.R
+++ inst/doc/multitree.R
@@ -1,11 +1,11 @@
-## ----echo=F--------------------------------------------------------------
+## ----echo=F-------------------------------------------------------------------
 knitr::opts_chunk$set(fig.width=6, fig.height=4) 
 
-## ---- library, message=FALSE---------------------------------------------
+## ---- library, message=FALSE--------------------------------------------------
 library(ape)
 library(TransPhylo)
 
-## ----simulate, results='hide'--------------------------------------------
+## ----simulate, results='hide'-------------------------------------------------
 neg <- 100/365
 off.r <- 1.5
 w.shape <- 10
@@ -20,11 +20,11 @@ simu1 <- simulateOutbreak(neg=neg, off.r=off.r, pi=pi, w.shape=w.shape,
 simu2 <- simulateOutbreak(neg=neg, off.r=off.r, pi=pi, w.shape=w.shape,
                          w.scale=w.scale, dateStartOutbreak=2000,dateT=2005)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 plot(simu1)
 plot(simu2)
 
-## ----results='hide'------------------------------------------------------
+## ----results='hide'-----------------------------------------------------------
 ptree1 <- extractPTree(simu1)
 ptree2 <- extractPTree(simu2)
 
@@ -37,7 +37,7 @@ record_tpj <- infer_multittree_share_param(list(ptree1,ptree2), w.shape, w.scale
                                     mcmcIterations = iters, thinning = thin, dateT = 2005,
                                     share = c("neg", "off.r", "off.p", "pi"))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 begin <- (iters * 0.5) / thin + 1
 end <- iters / thin
 record_tp1 <- record_tp1[begin:end]
@@ -45,7 +45,7 @@ record_tp2 <- record_tp2[begin:end]
 record_tpj[[1]] <- record_tpj[[1]][begin:end]
 record_tpj[[2]] <- record_tpj[[2]][begin:end]
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 get_param_estimates <- function(record, p){
   sapply(record, function(x) x[[p]])
 }
@@ -61,16 +61,16 @@ df <- data.frame(run = rep(c("tp1","tp2","tp_multitree"), each = length(record_t
                    get_param_estimates(record_tp2, "neg"),
                    get_param_estimates(record_tpj[[1]], "neg")))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 boxplot(df$pi~df$run,ylab='pi')
 lines(c(0,4),rep(0.8,2),col='grey')
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 boxplot(df$off.r~df$run,ylab='off.r')
 lines(c(0,4),rep(1.5,2),col='grey')
 
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 boxplot(df$neg~df$run,ylab='Ne*g')
 lines(c(0,4),rep(100/365,2),col='grey')
 
